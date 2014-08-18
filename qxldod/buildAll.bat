400f7ad7c5c75e98966c53b5d6b21c97360c5c63
Vadim Rozenfeld <vrozenfe@redhat.com>
2014-08-18 23:14:54 +1000

    update build to VS2012

--- qxldod/buildAll.bat
+++ qxldod/buildAll.bat
@@ -5,7 +5,7 @@ call clean.bat
 rem WIN8_64
 setlocal
 if exist Install\win8\amd64 rmdir Install\win8\amd64 /s /q
-call callVisualStudio.bat 11 qxldod.vcxproj /Rebuild "Win8 Release|x64" /Out buildfre_win8_amd64.log
+call callVisualStudio.bat 12 qxldod.vcxproj /Rebuild "Win8 Release|x64" /Out buildfre_win8_amd64.log
 mkdir .\Install\Win8\x64
 del /Q .\Install\Win8\x64\*
 copy /Y objfre_win8_amd64\amd64\qxldod.sys .\Install\Win8\x64
@@ -18,7 +18,7 @@ if %ERRORLEVEL% NEQ 0 goto :eof
 rem WIN8_32
 setlocal
 if exist Install\win8\x86 rmdir Install\win8\x86 /s /q
-call callVisualStudio.bat 11 qxldod.vcxproj /Rebuild "Win8 Release|Win32" /Out buildfre_win8_x86.log
+call callVisualStudio.bat 12 qxldod.vcxproj /Rebuild "Win8 Release|Win32" /Out buildfre_win8_x86.log
 mkdir .\Install\Win8\x86
 del /Q .\Install\Win8\x86\*
 copy /Y objfre_win8_x86\i386\qxldod.sys .\Install\Win8\x86
