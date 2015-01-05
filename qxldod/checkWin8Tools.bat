e4480e7d50e73c4d5fcf5ae1f59c5cd72a8fa8dc
Vadim Rozenfeld <vrozenfe@redhat.com>
2015-01-05 21:30:55 +1100

    fix VS 12 checking batch

--- qxldod/checkWin8Tools.bat
+++ qxldod/checkWin8Tools.bat
@@ -1,16 +1,8 @@
 @echo off
 
-reg query "HKLM\Software\Microsoft\Windows Kits\WDK" /v WDKProductVersion >nul 2>nul
-if %ERRORLEVEL% EQU 0 goto checkVS12
-reg query "HKLM\Software\Wow6432Node\Microsoft\Windows Kits\WDK" /v WDKProductVersion > nul 2>nul
-if %ERRORLEVEL% EQU 0 goto checkVS12
-echo ERROR building Win8 drivers: Win8 WDK is not installed
-exit /b 1
-
-:checkVS12
 reg query HKLM\Software\Microsoft\VisualStudio\12.0 /v InstallDir > nul 2>nul
 if %ERRORLEVEL% EQU 0 exit /b 0
 reg query HKLM\Software\Wow6432Node\Microsoft\VisualStudio\12.0 /v InstallDir > nul 2>nul
 if %ERRORLEVEL% EQU 0 exit /b 0
-echo ERROR building Win8 drivers: VS11 is not installed
+echo ERROR building Win8 drivers: VS12 is not installed
 exit /b 2
