23f76adeccfb7e9f02eafb1c78f0acd5cfd431b8
Sameeh Jubran <sameeh@daynix.com>
2016-08-09 15:21:23 +0300

    Upgrade to Windows 10 WDK
    * Added Windows 10 Configurations and removed older ones.
    
    * Migration to Visual Studio 2015.
    
    * Set Allow Date, Time and Timestamp property for all builds/platforms.
    
    * Fixed up project configuration names to be consistent.
    
    * unified build output directories
    
    * Simple build script added
    
    Signed-off-by: Dmitry Fleytman <dmitry@daynix.com>
    Acked-by: Frediano Ziglio <fziglio@redhat.com>

--- qxldod/checkWin8Tools.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-reg query HKLM\Software\Microsoft\VisualStudio\12.0 /v InstallDir > nul 2>nul
-if %ERRORLEVEL% EQU 0 exit /b 0
-reg query HKLM\Software\Wow6432Node\Microsoft\VisualStudio\12.0 /v InstallDir > nul 2>nul
-if %ERRORLEVEL% EQU 0 exit /b 0
-echo ERROR building Win8 drivers: VS12 is not installed
-exit /b 2
